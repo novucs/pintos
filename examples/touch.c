c8a91dea54b1ea2f62ed13e092ff1728f6e7fcba
novucs <contact@novucs.net>
2017-12-04 14:36:42 +0000

    Create touch command, useful for testing create system call

---
+++ examples/touch.c
@@ -0,0 +1,20 @@
+/* touch.c
+   Creates files specified on command line. */
+
+#include <stdio.h>
+#include <syscall.h>
+
+int
+main (int argc, const char **argv)
+{
+  bool success = true;
+  int i;
+
+  for (i = 1; i < argc; i++)
+    if (!create (argv[i], 8))
+      {
+        printf ("%s: create failed\n", argv[i]);
+        success = false;
+      }
+  return success ? EXIT_SUCCESS : EXIT_FAILURE;
+}
